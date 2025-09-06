e2cb0f124cfe970f8b1a0eba7d4e16372861fa61
Gabriel Siqueira <gabriel.siqueira3000@gmail.com>
2025-09-06 11:55:11 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -27,9 +28,14 @@ public class Program {
 		
 		System.out.println();
 		
-		System.out.println("=== TEST 3: seller findByDepartment ===");		
+		System.out.println("=== TEST 3: seller findAll ===");		
 		list = sellerDao.findAll();	
 		list.forEach(System.out::println);
+		
+		System.out.println("=== TEST 3: seller insert ===");		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
